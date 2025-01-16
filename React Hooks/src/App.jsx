92d89102ca10b8289d93046bf96b028bb47b1114
richmondafari <afaririchmond1000@gmail.com>
2025-01-16 06:22:59 +0000

    Use the use Ref hook to understand how to change state without re-rendering

--- React Hooks/src/App.jsx
+++ React Hooks/src/App.jsx
@@ -1,16 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [times, setTime] = useState(0);
+  const [value, setValue] = useState(0);
+  const count = useRef(0)
+useEffect( ()=> {
+ count.current = count.current + 1
+}, )
 
-  useEffect(() => {
-    setTimeout(() => {
-      setTime(times + 1);
-    }, 2000);
-  } , [times]);
-
-  return <h1>I have rendered {times} times</h1>;
+  return (
+    <>
+      <button
+        className="button-1"
+        onClick={() => {
+          setValue((prev) => prev - 1);
+        }}
+      >
+        -
+      </button>
+      <h1> {value}</h1>
+      <button
+        className="button-1"
+        onClick={() => {
+          setValue((prev) => prev + 1);
+        }}
+      >
+        +
+      </button>
+      <h1>Render Count: {count.current}</h1>
+    </>
+  );
 };
 
 export default App;
